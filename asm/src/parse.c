e9dbc0afa750f8ae85d001151e3100ac47eea1b2
Alexander PAVLYUCHUK <apavlyuc@e1r8p10.unit.ua>
2019-06-02 16:13:36 +0300

    add asm folder with init code

---
+++ asm/src/parse.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parse.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: apavlyuc <apavlyuc@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/06/02 15:39:31 by apavlyuc          #+#    #+#             */
+/*   Updated: 2019/06/02 16:11:45 by apavlyuc         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../inc/asm.h"
+#include "../../libft/libft.h"
+
+char		*parse(char const *file_content)
+{
+	char	*ret;
+
+	ret = (char *)file_content;
+	return (ret);
+}

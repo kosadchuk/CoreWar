fe0514df25d0c1999fb4842b5f1fc968d3ac9334
Oleksandr Pavliuchuk <oleksandr.pavliuchuk@thomsonreuters.com>
2019-06-18 13:30:32 +0300

    create body for ::write_commands_in_binary; add dummy ::init_by_command_data

--- asm/src/writer/write_commands.c
+++ asm/src/writer/write_commands.c
@@ -17,8 +17,29 @@
 #include <stdio.h>
 #include <unistd.h>
 
-void		write_commands_in_binary(int fd, t_asm const *content)
+void			init_by_command_data(t_asm const *src, t_command const *com, void **dst, t_ull *len)
 {
-	(void)fd;
-	(void)content;
+	(void)src;
+	(void)com;
+	(void)dst;
+	(void)len;
+}
+
+void			write_commands_in_binary(int fd, t_asm const *content)
+{
+	t_command	*command;
+	t_list		*lst;
+	void		*data;
+	t_ull		len;
+
+	lst = content->code->commands;
+	while (lst)
+	{
+		data = 0;
+		len = 0;
+		command = (t_command *)lst->content;
+		init_by_command_data(content, command, &data, &len);
+		len = write(fd, data, len);
+		lst = lst->next;
+	}
 }

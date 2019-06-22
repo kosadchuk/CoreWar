e27b5b1dfab9a9192061f10868958cb9d3093d32
apavlyuc <apavlyuc@gmail.com>
2019-06-22 15:32:49 +0300

    fix bug (replace char type by unsigned char type)

--- asm/show_file.cc
+++ asm/show_file.cc
@@ -10,13 +10,15 @@ int		main(int ac, char **av)
 	std::ifstream ifd(av[1], std::ios::binary | std::ios::ate);
 	int size = ifd.tellg();
 	ifd.seekg(0, std::ios::beg);
-	std::vector<char> buffer;
+	std::vector<unsigned char> buffer;
 	buffer.resize(size);
-	ifd.read(buffer.data(), size);
+	ifd.read((char *)buffer.data(), size);
 
 	for (int i = 2192; i < buffer.size(); ++i)
 	{
 		std::cout << std::setfill('0') << std::setw(2) << std::hex << (int)buffer[i];
+		if (i % 2 && i != 2192)
+			std::cout << std::endl;
 	}
 	std::cout << "\n";
 

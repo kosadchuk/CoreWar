ec2dc5063e42367b705a10b26a810400fa8c9433
Alexander PAVLYUCHUK <apavlyuc@e1r5p11.unit.ua>
2019-06-28 19:30:23 +0300

    normed

--- asm/src/writer/writer.c
+++ asm/src/writer/writer.c
@@ -6,7 +6,7 @@
 /*   By: apavlyuc <apavlyuc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/02 14:01:36 by apavlyuc          #+#    #+#             */
-/*   Updated: 2019/06/02 16:11:40 by apavlyuc         ###   ########.fr       */
+/*   Updated: 2019/06/28 19:14:26 by apavlyuc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,21 +18,20 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 
-void		write_file(char const* file_name, t_asm *content)
+void		write_file(char const *file_name, t_asm *content)
 {
 	int		fd;
 
 	if (!validate_parsed_data(content))
 	{
 		ft_memdel((void **)file_name);
-		return;
+		return ;
 	}
 	if ((fd = open(file_name, O_WRONLY | O_CREAT, S_IRWXU)) < 0)
 	{
 		ft_putendl("Error: can't create file");
-		return;
+		return ;
 	}
-	//write(fd, "hello", 5);
 	write_header_in_binary(fd, content);
 	write_commands_in_binary(fd, content);
 }
